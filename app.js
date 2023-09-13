403631ffcd1859b80ed1c86b42ee965164908892
KreeDmaste4 <142033862+KreeDmaste4@users.noreply.github.com>
2023-09-13 14:04:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var x = +prompt('Введите число') 
 var y = +prompt('Введите число') 
 
-var otv = ( 'x' + 'y' = ) 
+var otv = ( x + y ) 
 alert (otv)
 
-console.log(x)
-console.log(y)
-console.log(otv)
